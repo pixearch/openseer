0bc13094366d86a3cd9764f0b4796578dfb0347e
carnealse <70449042+carnealse@users.noreply.github.com>
2026-04-04 23:47:09 -0400

    FIX: add new fixes

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,15 @@
 import type { NextConfig } from "next";
+import path from "path";
+import { createRequire } from "node:module";
 
-const nextConfig: NextConfig = {};
+const require = createRequire(import.meta.url);
+const appRoot = path.dirname(require.resolve("./package.json"));
+
+const nextConfig: NextConfig = {
+  outputFileTracingRoot: appRoot,
+  turbopack: {
+    root: appRoot,
+  },
+};
 
 export default nextConfig;
